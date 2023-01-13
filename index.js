2c43ccc79b7ac1bcc81dceaf169f6c90dd4247ec
Stephen Vizzusi <svizzusi13@gmail.com>
2023-01-13 14:38:00 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+let event = "surprise"
+const cardsArray = []
+
+function writeCards(cards, event){
+  for (let i = 0; i < cards.length; i++) {
+    cardsArray.push (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+  }
+  
+  return cardsArray
+}
+
+writeCards();
+
+
+
+let num = 10
+function countDown(num){
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
+countDown();
